9677f7151934e2010a9a2748f8210be2ea0edf20
bhavy-2511 <f20240788@goa.bits-pilani.ac.in>
2026-08-22 21:47:53 +0530

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l = 0;
+        int r =1;
+        int profit=0;
+        while (r < prices.size()){
+            if (prices[l] > prices[r]){
+                l=r;
+                r=r+1;
+            }
+            else if (prices[l] <= prices[r]){
+                profit= max(profit,prices[r]-prices[l]);
+                r=r+1;
+            }
+        }
+        return profit;
+    }
+};

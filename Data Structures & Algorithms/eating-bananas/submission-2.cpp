2b26101639af98ca66097a242ab5e68a6327b5e0
bhavy-2511 <f20240788@goa.bits-pilani.ac.in>
2026-08-30 00:50:49 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int r = *max_element(piles.begin(),piles.end());
+        int l=1;
+        int res=r;
+
+        while (l<r){
+            int m =l+(r-l)/2;
+            long long test=0;
+            for (int i =0;i<piles.size();i++){
+                test=test+((piles[i]-1)/m)+1;
+            }
+            if (test<=h){
+                r=m;
+            }
+            else if (test>h){
+                l=m+1;
+            } 
+        }
+        return (l+r)/2;
+    }
+};
